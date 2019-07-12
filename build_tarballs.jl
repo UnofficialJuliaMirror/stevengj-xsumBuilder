2f4a77e3b65e736a90bd0d5e94362f2abc48e30c
Steven G. Johnson <stevenj@alum.mit.edu>
2019-07-12 12:28:26 -0400

    first commit

---
+++ build_tarballs.jl
@@ -0,0 +1,36 @@
+using BinaryBuilder
+
+# Collection of sources required to build Blosc
+sources = [
+    "https://gitlab.com/radfordneal/xsum.git" =>
+    "63bd4c13e4a8da4932ded362e2921ed99079f432", # version 2018-12-02 "1.0"
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/xsum
+${CC} ${LDFLAGS} -shared -fPIC -O3 -mfpmath=sse -fno-inline-functions -o ${libdir}/libxsum.${dlext}
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms() # build on all supported platforms
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libxsum", :libxsum),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "xsum", v"1.0", sources, script, platforms, products, dependencies)
+
+println("Contents of ", pwd(), " = ", readdir("."))
+if isdir("products")
+    println("PRODUCTS = ", readdir("products"))
+else
+    println("products/ directory not found")
+end
